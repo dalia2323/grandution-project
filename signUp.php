1c58a18b85481ea33fbc1d64f8bdd7be2cd1b489
dalia2323 <d.k.saleem@students.ptuk.edu.ps>
2023-03-18 19:31:32 +0200

    signUp validation #4

--- signUp.php
+++ signUp.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
-require_once('handler/db.php');
+include('handler/db.php');
+if(isset($_SESSION['user'])){
+    header('location:cityGuide.php');
+    exit();
+}
 if(isset($_POST['submit'])){
 $name=filter_var($_POST['username'],FILTER_SANITIZE_STRING);
 $email=filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
 $password=filter_var($_POST['password'],FILTER_SANITIZE_STRING);
-}
+$cofPassword=filter_var($_POST['ConfPassword'],FILTER_SANITIZE_STRING);
 $errors=[];
 //validate name
 if(empty($name))
@@ -42,9 +46,12 @@ if(empty($password))
     $errors[]="You should enter Password";
 }
 elseif(strlen($password)<8){
-    $errors[]="Password should be 8 or more";
+    $errors[]="Password must be at least 8 character long";
 }
+elseif(($password)!=($cofPassword)){
+    $errors[]="please make sure your passwords match";
 
+}
 
 if(empty($errors)){
     $password=password_hash($password,PASSWORD_DEFAULT);
@@ -59,18 +66,12 @@ if(empty($errors)){
     header('location:cityGuide.php');
 }
 
-
-else{
-    var_dump($errors);
 }
-//INSERT
-
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -79,48 +80,62 @@ else{
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Sign up</title>
-        <!-- add icon -->
-        <link rel="icon" type="image/x-icon" href="img/icon.png">
+    <!-- add icon -->
+    <link rel="icon" type="image/x-icon" href="img/icon.png">
+
 </head>
+
 <body class="login">
     <!-- Div 1 first -->
     <div class="first">
         <!-- Div2 second -->
         <div class="second secondsignup">
-           
-    </div>
+
+        </div>
         <!-- Div3 third -->
         <div class="third">
             <form action="signUp.php" method="post">
-                    <h2>Get started</h2></br>
-                    <p>Create your own account to be able to enter the site.</p></br>
-                   
-         <input type="text" class="icon1" name="username" placeholder="      Enter your  Name" required
-          value="<?php if(isset($_POST['username'])){ echo $_POST['username'];}?>" > <br>
-                    
-          <input type="email" class="icon2" name="email" placeholder="      Enter your  Email" 
-          required value="<?php if(isset($_POST['email'])){ echo $_POST['email'];}?>" >
-                    <br>
-                  
-                    <input type="password"  class="icon3" placeholder='        password'  
-                    name='password'  /></br>
-                    
-                    
-                    <input id="show" type="password" class="icon3" name="Cpass"
-                     placeholder="        conform password"><br>
-                    <button type="submit" name="submit" class="scompany button"><a>sign up</a></button> 
-                    <!-- <button type="submit" name="signupcompany"class="scompany button"><a>Iam Company</a></button>  -->
-            </br> 
-        </br>
-                    <a class="Imcompany" href="signupcompany.html">Im company?</a>
+                <h2>Get started</h2></br>
+                <p>Create your own account to be able to enter the site.</p></br>
+
+                <input type="text" class="icon1" name="username" placeholder="      Enter your  Name" required
+                    value="<?php if(isset($_POST['username'])){ echo $_POST['username'];}?>"> <br>
+
+                <input type="email" class="icon2" name="email" placeholder="      Enter your  Email" required
+                    value="<?php if(isset($_POST['email'])){ echo $_POST['email'];}?>">
+                <br>
+
+                <input type="password" class="icon3" placeholder='        password' name='password' /></br>
+
+
+                <input id="show" type="password" class="icon3" name="ConfPassword"
+                    placeholder="        conform password"><br>
+                <div style="color:red;">
+                    <?php 
+                      if(isset($errors)){
+                      if(!empty($errors)){
+                       foreach($errors as $msg){
+                        echo $msg . "<br>";
+                        }
+                      }
+                  }?>
+                </div>
+
+                <button type="submit" name="submit" class="scompany button"><a>sign up</a></button>
+                <!-- <button type="submit" name="signupcompany"class="scompany button"><a>Iam Company</a></button>  -->
+                </br>
+                </br>
+
+                <a class="Imcompany" href="signupcompany.html">Im company?</a>
                 <a class="Imcompany" href="login.html">Do you Have account?</a>
 
             </form>
 
         </div>
-        
+
 
     </div>
-    
+
 </body>
+
 </html>
