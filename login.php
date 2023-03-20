2dc97e70e9331aa9e57764e7927b5d949a5f9dff
dalia2323 <d.k.saleem@students.ptuk.edu.ps>
2023-03-20 21:54:06 +0200

    Login validation

--- login.php
+++ login.php
@@ -1,3 +1,61 @@
+<?php
+session_start();
+include('handler/db.php');
+if(isset($_POST['login']))
+{
+    global $conn;
+    $email=filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+    $password=filter_var($_POST['password'],FILTER_SANITIZE_STRING);
+    $errors=[];
+ 
+    if(empty($email))
+    {
+        $errors[]="You should enter email ";
+    }
+    
+    if(!empty($email)){
+        $stm="SELECT * FROM users WHERE email='$email' ";
+        $q=$conn->prepare($stm);
+        $q->execute();
+        $data=$q->fetch();
+        $hashed_pass=$data['password'];
+        $p=password_verify($password,$hashed_pass);
+        if(!$data AND !$p){
+            $errors[] = "incorrect email";
+
+        }
+    }
+     
+  if(!$data){
+    $errors[]="invalid login";
+  }
+  else{
+
+    $_SESSION['user']=[
+        "name"=>$data['name'],
+        "email"=>$email,
+      ];
+    header('location:cityGuide.php');
+  }
+  
+  
+  
+    }
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,6 +86,18 @@
                     <input type="text"  class="icon2" placeholder='      Email'  name='email' /></br>
                     
                     <input type="password" class="icon3" placeholder='      password'  name='password' /></br>
+                    <div style="color:red;">
+                    <?php 
+                      if(isset($errors)){
+                      if(!empty($errors)){
+                       foreach($errors as $msg){
+                        echo $msg . "<br>";
+                        }
+                      }
+                  }?>
+                </div>
+                    
+                    
                     <button type="submit" name="login" class="button"><a>login</a></button>
             </br> </br></br>
         
