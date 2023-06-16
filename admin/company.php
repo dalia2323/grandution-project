3aa60753c96995bec18ddf2da08d8daf5f059a39
dalia2323 <d.k.saleem@students.ptuk.edu.ps>
2023-06-16 12:21:32 +0300

    company

--- admin/company.php
+++ admin/company.php
@@ -5,7 +5,7 @@ include('../handler/db.php');
 ?>
 <?php
 session_start();
-if(!isset($_SESSION['admin'])){
+if(!isset($_SESSION['company'])){
   header('location:admin-login.php');
   exit();
 
@@ -92,31 +92,13 @@ select{
      
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="admin.php">City guide </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item ">
-                  <a class="nav-link" href="handel-user.php">user</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="handel-company.php">Company</a>
-                </li>
-                <li class="nav-item active">
-                  <a class="nav-link" href="show.php">Shops</a>
-                </li>
-                <li class="nav-item ">
-                  <a class="nav-link" href="addtrend.php">addtrend</a>
-                </li>
-                <li class="nav-item ">
-                  <a class="nav-link" href="show-trendshop.php">Trend shop</a>
+   
 </li>
             </ul>
             <ul class="navbar-nav ml-auto mr-5">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <?php echo $_SESSION['admin']['name'];?>
+                    <?php echo $_SESSION['company']['name'];?>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="admin-profile.php">Profile</a>
